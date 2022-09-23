41bf4d27ad05f1a216084c3c085a2d298a47cede
Angel Moreno <113930724+Amoreno73@users.noreply.github.com>
2022-09-23 15:27:34 +0000

    I added string to the print count lines. Now it says: "The first letter appeared x times."

--- main.py
+++ main.py
@@ -19,18 +19,12 @@
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 # =-=-=-=-=-=-=-=-=-
 userText = input("Enter any amount of text: ")
-# this will turn the user text into small letters so it does not mess up count
-userText.lower()
-print(userText)
+
 enterLetters = input("Enter any three letters: ") 
 threeLetters = list(enterLetters)
 print(threeLetters)
 # ===================================================
-print(userText.count(threeLetters[0]))
-print(userText.count(threeLetters[1]))
-print(userText.count(threeLetters[2]))
+print("The first letter appeared " + str(userText.count(threeLetters[0])) + " times.")
+print("The second letter appeared " + str(userText.count(threeLetters[1])) + " times.")
+print("The third letter appeared " + str(userText.count(threeLetters[2])) + " times.")
 
-
-
-
-print(userText)
